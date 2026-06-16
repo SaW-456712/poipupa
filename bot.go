1b7567ddc7ef6cd13f2992c7cf83229358e01390
SAW <iamvampir@proton.me>
2026-06-16 23:38:54 +0300

    Add files via upload

---
+++ bot.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	//"os"
+
+	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	// Bot Object make and Error chto-to tam
+	bot , err := tgbotapi.NewBotAPI("8953206131:AAEfpVc95VHc-NiAOxRmdyi6_PxJ716ZVe8");
+	if err != nil {
+		log.Panic("bot create error: ", err);
+	}
+	// Debug +
+	bot.Debug = true;
+	log.Printf(bot.Self.UserName);
+
+	// Настраиваем Long Polling
+	u := tgbotapi.NewUpdate(0);
+	u.Timeout = 60;
+
+	// Channel update 
+	updates := bot.GetUpdatesChan(u);
+
+	// Message get and answer
+	for update := range updates {
+		if update.Message == nil { continue } // if it not a massage ignore (maybe user input in group)
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text); // logging anything in console
+	
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hi!"); // ID - КОМУ ОТПРАВЛЯТЬ TEXT - ЧТО ОТПРАВИТЬ
+		msg.ReplyToMessageID = update.Message.MessageID; // rEPLY MESSAGE 
+
+
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Println(err);
+		}
+
+	}
+
+}
